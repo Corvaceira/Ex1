24d8b05d812fa3ba4c24faa7ad9f5d2ec1d976cb
Corvaceira <aluno81797@ipt.pt>
2022-06-09 00:15:45 +0100

    First commit

---
+++ Setup.py
@@ -0,0 +1,10 @@
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name="arithmetic",
+    version="0.1",
+    description=" Funçoesarithmetic",
+    packages=find_packages(),
+    include_package_data=True
+)
